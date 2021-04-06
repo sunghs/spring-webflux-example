e83919377bfdce96698719728f11cfce9cc86b2b
sunghs <sunghs9071@naver.com>
2021-04-06 13:11:48 +0900

    Initial commit

---
+++ src/test/java/sunghs/springwebfluxexample/SpringWebfluxExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package sunghs.springwebfluxexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
