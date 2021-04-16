5ac8c9b2a239a69316ce347b9c7241a0e05c78e4
sunghs <sunghs9071@naver.com>
2021-04-16 16:46:22 +0900

    h2 db 에 기본으로 넣을 데이터 추가, mode 를 mysql로 변경

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS user_info;
+
+CREATE TABLE user_info(seq INT PRIMARY KEY auto_increment, name VARCHAR(255));
+
+INSERT INTO user_info(name) VALUES('name1');
+INSERT INTO user_info(name) VALUES('name2');
+INSERT INTO user_info(name) VALUES('name3');
+INSERT INTO user_info(name) VALUES('name4');
+INSERT INTO user_info(name) VALUES('name5');
+INSERT INTO user_info(name) VALUES('name6');
+INSERT INTO user_info(name) VALUES('name7');
+INSERT INTO user_info(name) VALUES('name8');
+INSERT INTO user_info(name) VALUES('name9');
+INSERT INTO user_info(name) VALUES('name10');
+INSERT INTO user_info(name) VALUES('name11');
+INSERT INTO user_info(name) VALUES('name12');
+INSERT INTO user_info(name) VALUES('name13');
+INSERT INTO user_info(name) VALUES('name14');
+INSERT INTO user_info(name) VALUES('name15');
+INSERT INTO user_info(name) VALUES('name16');
+INSERT INTO user_info(name) VALUES('name17');
+INSERT INTO user_info(name) VALUES('name18');
+INSERT INTO user_info(name) VALUES('name19');
+
+
