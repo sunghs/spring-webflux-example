b7e647067fde19756c50d84e1091b8efa98b50b3
sunghs <sunghs9071@naver.com>
2021-04-16 16:36:57 +0900

    h2 연결 에러 추가

--- src/main/java/sunghs/springwebfluxexample/config/H2Config.java
+++ src/main/java/sunghs/springwebfluxexample/config/H2Config.java
@@ -3,26 +3,32 @@ package sunghs.springwebfluxexample.config;
 import java.sql.SQLException;
 import org.h2.tools.Server;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 
 @Configuration
+@Profile("local")
 public class H2Config {
 
-    private final Server server;
+    private final Server webServer;
+    private final Server tcpServer;
 
     public H2Config() throws SQLException {
-        server = Server.createWebServer("-webPort", "8081", "-tcpAllowOthers");
+        webServer = Server.createWebServer("-webPort", "8081", "-tcpAllowOthers");
+        tcpServer = Server.createTcpServer("-tcpPort", "9091", "-tcpAllowOthers");
     }
 
     @EventListener(ContextRefreshedEvent.class)
     public void h2Start() throws SQLException {
-        server.start();
+        webServer.start();
+        tcpServer.start();
     }
 
     @EventListener(ContextClosedEvent.class)
     public void h2Stop() {
-        server.stop();
+        webServer.stop();
+        tcpServer.stop();
     }
 }
